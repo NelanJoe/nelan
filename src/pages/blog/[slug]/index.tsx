d690c055e70609baf2073e58badf84723384168e
NelanJoe <nelanjorelat@gmail.com>
2025-11-10 08:16:42 +0700

    feat: post detail

--- src/pages/blog/[slug]/index.tsx
+++ src/pages/blog/[slug]/index.tsx
@@ -1,13 +1,16 @@
-import { allPosts } from "content-collections";
-import { MDXContent } from "@content-collections/mdx/react";
+import Link from "next/link";
+import Image from "next/image";
 import type {
   GetServerSidePropsContext,
   GetStaticPaths,
   InferGetServerSidePropsType,
 } from "next";
+import { allPosts } from "content-collections";
+import { MDXContent } from "@content-collections/mdx/react";
+import { ArrowLeftIcon } from "lucide-react";
 
 import DefaultLayout from "@/layouts/default";
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export const getStaticPaths = (async () => {
   const paths = allPosts.map((post) => ({
@@ -43,15 +46,34 @@ export default function Post({
 }: InferGetServerSidePropsType<typeof getStaticProps>) {
   return (
     <DefaultLayout title={post.title}>
-      <section className="py-10">
-        <div className="mb-10">
-          <h2 className="animate-in fade-in text-center text-2xl leading-relaxed font-semibold duration-150">
-            {post.title}
-          </h2>
+      <section>
+        <div className="my-10 px-4.5">
+          <Button variant="link" asChild size="sm">
+            <Link href="/blog" className="inline-flex items-center gap-2">
+              <ArrowLeftIcon />
+              <span>Back</span>
+            </Link>
+          </Button>
         </div>
-        <article className="prose dark:prose-invert prose-h1:font-bold prose-a:text-blue-600 prose-p:text-justify prose-img:rounded-md prose-headings:underline mx-auto">
-          <MDXContent code={post.mdx} />
-        </article>
+        <div className="space-y-4">
+          <div>
+            <h2 className="animate-in fade-in text-center text-2xl leading-relaxed font-semibold duration-150">
+              {post.title}
+            </h2>
+          </div>
+          <div className="my-16">
+            <Image
+              src={post.cover}
+              alt="post-cover"
+              width={200}
+              height={200}
+              className="h-96 w-full rounded-md object-cover"
+            />
+          </div>
+          <article className="prose dark:prose-invert prose-h1:font-bold prose-a:text-blue-600 prose-p:text-justify prose-img:rounded-md prose-headings:underline mx-auto pb-10">
+            <MDXContent code={post.mdx} />
+          </article>
+        </div>
       </section>
     </DefaultLayout>
   );
