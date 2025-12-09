da346de6b813234167bd0e79d3fd66186c1f9d11
NelanJoe <nelanjorelat@gmail.com>
2025-12-09 20:47:03 +0700

    feat: error page

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -3,28 +3,31 @@ import { NextPageContext } from "next";
 import { Icon } from "@iconify/react";
 import DefaultLayout from "@/layouts/default";
 import { Button } from "@/components/ui/button";
+import { Empty, EmptyContent } from "@/components/ui/empty";
 
 export default function Error({ statusCode }: { statusCode?: number }) {
   return (
     <DefaultLayout title={`${statusCode} - Error`}>
-      <section className="grid min-h-dvh place-content-center space-y-8">
-        <div>
-          <h2 className="prose text-center text-8xl leading-relaxed font-semibold">
-            {statusCode}
-          </h2>
-          <p className="text-center">
-            {statusCode
-              ? `An error ${statusCode} occurred on server`
-              : "An error occurred on client"}
-          </p>
-        </div>
-        <Button size="sm" asChild>
-          <Link href="/">
-            <Icon icon="mynaui-arrow-left" fontSize={24} />
-            Back to Home
-          </Link>
-        </Button>
-      </section>
+      <Empty>
+        <EmptyContent>
+          <div className="space-y-5">
+            <h2 className="prose text-center text-6xl font-semibold">
+              {statusCode}
+            </h2>
+            <p className="text-center">
+              {statusCode
+                ? `An error ${statusCode} occurred on server`
+                : "An error occurred on client"}
+            </p>
+            <Button size="sm" asChild>
+              <Link href="/">
+                <Icon icon="mynaui-arrow-left" fontSize={24} />
+                Back to Home
+              </Link>
+            </Button>
+          </div>
+        </EmptyContent>
+      </Empty>
     </DefaultLayout>
   );
 }
